18e0655853423c7efb1720c482218786ae5732a5
Khushi Mehra <akmkhushimehra02@gmail.com>
2025-12-20 01:59:49 +0530

    Fix backend render deployment

--- backend/Server.js
+++ backend/Server.js
@@ -22,12 +22,14 @@ app.get("/", (req, res) => {
   res.send("ShowcaseHub backend running");
 });
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(process.env.PORT || 5000, () => {
-      console.log("Server running on port 5000");
+      console.log("Server running on port {PORT}");
     });
   })
   .catch((err) => console.log(err));
