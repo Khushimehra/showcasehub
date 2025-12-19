49d902c30a40f880ffc068b4e9bf691d07fa5f6b
Khushi Mehra <akmkhushimehra02@gmail.com>
2025-12-20 02:03:41 +0530

    Fix backend render deployment

--- backend/Server.js
+++ backend/Server.js
@@ -25,11 +25,13 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("Server running on port {PORT}");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
